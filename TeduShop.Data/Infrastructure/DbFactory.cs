e4472ba8e0af1a13ae3962fbca61c5cbefbdebf4
vancanh3 <truongvancanhdtu@gmail.com>
2022-10-26 21:04:03 +0700

    add Db context

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if(dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable //tu dong huy ca phuong thuc
+    {
+        private bool isDisposed;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if(!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            isDisposed = true;
+        }
+
+        //Overide this to dispose custom objects
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
